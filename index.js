7c796b16af7096a7bd820ccab7d5ec2e3c93b9a3
Shubham Soni <ssoni6359@gmail.com>
2024-12-19 16:45:57 +0530

    Using my published package

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const nullFunc = require('gittesting');
+
 function isNullOrEmpty(input) {
     return (input === undefined || input === null || input === '');
 }
-
 module.exports = isNullOrEmpty;
+
+
+console.log("From Local Function: ", isNullOrEmpty(null));
+console.log("From Local Function: ", isNullOrEmpty("Check"));
+console.log("==============================")
+console.log("From Package Function: ", nullFunc(null));
+console.log("From Package Function: ", nullFunc("Check"));
